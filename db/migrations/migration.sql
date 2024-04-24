ef3f3389f7cc0baa5de7faf97792fbd2da19e739
Dmitriy Dodon <47043438+DmitriyDodon@users.noreply.github.com>
2024-04-24 21:14:41 +0300

    Update migration.sql

--- db/migrations/migration.sql
+++ db/migrations/migration.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS cars (
-    id U			UID PRIMARY KEY,
+    id 			UUID PRIMARY KEY,
     color        	VARCHAR(255) NOT NULL,
-	price_in_cents 	BIGINT NOT NULL,
-	max_speed_mph  	INT,
-	max_speed_kmp 	INT NOT NULL,
-	vendor_name   	VARCHAR(255) NOT NULL,
-	model_name   	VARCHAR(255) NOT NULL,
+    price_in_cents 	BIGINT NOT NULL,
+    max_speed_mph  	INT,
+    max_speed_kmp 	INT NOT NULL,
+    vendor_name   	VARCHAR(255) NOT NULL,
+    model_name   	VARCHAR(255) NOT NULL,
     created_at 		TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
